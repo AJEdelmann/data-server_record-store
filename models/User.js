5ae91cb9bed3e8c4e041bc505b88b7ec10d99da6
ajedelmann <albert.edelmann@gmail.com>
2019-12-02 11:41:33 +0100

    JWT creation on user POST

--- models/User.js
+++ models/User.js
@@ -1,50 +1,77 @@
 const mongoose = require("mongoose");
-const {
-  Schema
-} = mongoose;
+const { Schema } = mongoose;
 const Address = require("./Address");
+const jwt = require("jsonwebtoken");
 
-const UserSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true
+const UserSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true
+    },
+    lastName: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    birthday: {
+      type: Date
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    address: {
+      type: Address,
+      required: true
+    },
+    tokens: [
+      {
+        access: {
+          type: String,
+          required: true
+        },
+        token: {
+          type: String,
+          required: true
+        }
+      }
+    ]
   },
-  lastName: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  birthday: {
-    type: Date
-  },
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  address: {
-    type: Address,
-    required: true
+  {
+    toJSON: {
+      virtuals: true
+    },
+    toObject: {
+      virtuals: true
+    }
   }
-}, {
-  toJSON: {
-    virtuals: true
-  },
-  toObject: {
-    virtuals: true
-  }
+);
+
+UserSchema.virtual("fullname").get(function() {
+  return this.firstName + " " + this.lastName;
 });
 
-UserSchema.virtual('fullname').get(function () {
-  return this.firstName + ' ' + this.lastName;
-});
+UserSchema.methods.generateAuthToken = function() {
+  const user = this;
+  const access = "x-auth";
+
+  const token = jwt
+    .sign({ id: user._id.toHexString(), access }, "babylon")
+    .toString();
+
+  user.tokens.push({ access, token });
+
+  return token;
+};
 
 module.exports = mongoose.model("User", UserSchema);
